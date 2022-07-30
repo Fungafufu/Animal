8e92f9cabcd29e804d8929f84dc07c5bc29e4d55
Fungafufu <108220098+Fungafufu@users.noreply.github.com>
2022-07-30 16:49:30 +0200

    Update jscripts.js

--- js/jscripts.js
+++ js/jscripts.js
@@ -7,3 +7,49 @@ function myFunction() {
       navhide.style.display = "block";
     }
   }
+
+
+  /*function fadein(){
+    document.getElementById("img").style.opacity = 0;
+  }
+
+  function fadeout(){
+    document.getElementById("img").style.opacity = 1;
+  }*/
+
+
+  document.getElementById("btn1").addEventListener("click", function()
+  {
+    var box1 = document.getElementById("animal1");
+    if(box1.style.display=="none")
+    {
+      box1.style.display="block";
+    }
+    else {
+      box1.style.display="none";
+    }
+  })
+
+  document.getElementById("btn2").addEventListener("click", function()
+  {
+    var box2 = document.getElementById("animal2");
+    if(box2.style.display=="none")
+    {
+      box2.style.display="block";
+    }
+    else {
+      box2.style.display="none";
+    }
+  })
+
+  document.getElementById("btn3").addEventListener("click", function()
+  {
+    var box3 = document.getElementById("animal3");
+    if(box3.style.display=="none")
+    {
+      box3.style.display="block";
+    }
+    else {
+      box3.style.display="none";
+    }
+  })
